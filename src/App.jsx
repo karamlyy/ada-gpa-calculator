445ef45cdba9741617f37f28fa924fee8d792782
Karam K. Afandi <77574018+karamlyy@users.noreply.github.com>
2024-06-10 14:59:59 +0400

    author added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import './App.css'
-import Calculator from './components/Calculator'
+import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
-    
-    <Calculator />
+    <div className="app-container">
+      <Calculator />
+      <footer className="author-section">
+        <p>Created by <a href="https://www.linkedin.com/in/karamlyy/">Karam Afandi</a> &copy; 2024</p>
+      </footer>
+    </div>
   );
 }
 
-export default App
+export default App;
